6a909442308b62b07df0f7a20ba3e023df01a03b
Renato Lobo R. Jr <renatolobojr@gmail.com>
2022-02-02 03:13:32 -0300

    redirecionamento para https

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,10 @@ const express = require('express');
 
 const routes = express.Router();
 
+routes.get("*", function(request, response, next){
+    response.redirect("https://" + request.headers.host + request.url);  
+  });
+
 routes.get('/', (req,res,next) => {
     return res.json({hello: 'world'});
 })
